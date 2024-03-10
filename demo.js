129374fdca4b9cabfcd9d8564aff2f3e83192c1c
Dat Le <leduydatfml@gmail.com>
2024-03-10 09:46:12 +0700

    Init html app

---
+++ demo.js
@@ -0,0 +1,16 @@
+// console.log('hello world from html');
+// let sum = (a, b)=>{
+//     return a+b;
+// }
+
+// console.log('check sum', sum(3, 6));
+
+// let obj={
+//     name: 'Baca',
+//     address: 'Tran khanh du',
+//     getName: function(){
+//         return this.address;
+//     }
+// }
+
+// console.log('show name: ', obj.getName())
