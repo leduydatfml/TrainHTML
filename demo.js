f8a127577f8c79fb1a5d78aafdf6db8c2a666554
Dat Le <leduydatfml@gmail.com>
2024-03-12 21:00:29 +0700

    fixheader

--- demo.js
+++ demo.js
@@ -14,3 +14,32 @@
 // }
 
 // console.log('show name: ', obj.getName())
+
+window.onload = function () {
+    let btnSearch = document.getElementById("button-search-form");
+    console.log('btnSearch', btnSearch)
+    const textInput = document.getElementById("input-first");
+    let msg = document.getElementById("msg");
+    
+    textInput.addEventListener("change", (e) => {
+        console.log("callll");
+
+        e.preventDefault();
+        formValidation();
+      });
+
+    btnSearch.addEventListener("submit", (e) => {
+        e.preventDefault();
+        formValidation();
+      });
+    
+      const formValidation = () => {
+        if (textInput.value === "") {
+          console.log("failure");
+          msg.innerHTML = "Task cannot be blank";
+        } else {
+          console.log("success");
+          msg.innerHTML = "";
+        }
+      };
+};
